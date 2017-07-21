7e3c5189b2de3135768bb165a470bcccf4cebccf
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-07-21 15:45:01 -0500

    Cleaned up regex is kinda working

--- strings.js
+++ strings.js
@@ -9,7 +9,6 @@ let output1 = document.querySelector('output1');
 let output2 = document.querySelector('output2');
 
 
-
 function reversal() {
     output1.innerHTML = input.value.split('').reverse().join('')
     }
@@ -28,25 +27,25 @@ input.addEventListener('keypress', (e) => {
 
 
 
-    let key = e.which || e.keyCode
+    let key = e.which || e.keyCode;
     if (key === 13) {
-        testString = input.value
+        testString = input.value;
         reversal(testString);
         alphabits(testString);
         palindrome(testString);
 
     }
-})
+});
 
 input.addEventListener('keypress', (e) => {
-    let key = e.which || e.keyCode
+    let key = e.which || e.keyCode;
     if (key === 13) {
-        testString = input.value
+        testString = input.value;
         reversal(testString);
         alphabits(testString);
         palindrome(testString);
     }
-})
+});
 
 
 
